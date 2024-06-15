ead9bbbb1e5238764e2e52c245775cfb9416576f
David Cobbina <davidcobbina47@gmail.com>
2024-06-15 16:54:35 +0200

    feat: add style to text stack

--- packages/swiftcommerce_ui/lib/widgets/text_stack.dart
+++ packages/swiftcommerce_ui/lib/widgets/text_stack.dart
@@ -4,9 +4,11 @@ class TextStack extends StatelessWidget {
   const TextStack({
     super.key,
     required this.text,
+    this.style,
   });
 
   final String text;
+  final TextStyle? style;
 
   @override
   Widget build(BuildContext context) {
